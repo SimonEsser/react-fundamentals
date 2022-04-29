4691a5c6b324075594367851e75fee8acb3ce059
s.esser <simon.esser@datamediq.com>
2022-04-29 16:56:05 +0200

    optimize extra credit 2 for exercise 05

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,26 +14,29 @@ import '../box-styles.css'
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
 
-const Box = ({style = {}, className = '', ...props}) => (
-  <div
-    className={`box ${className}`}
-    style={{...style, fontStyle: 'italic'}}
-    {...props}
-  ></div>
-)
+const Box = ({size, style = {}, ...props}) => {
+  const sizeClassName = size ? `box--${size}` : ''
+  return (
+    <div
+      className={`box ${sizeClassName}`}
+      style={{...style, fontStyle: 'italic'}}
+      {...props}
+    />
+  )
+}
 
 const smallBox = (
-  <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+  <Box size="small" style={{backgroundColor: 'lightblue'}}>
     small lightblue box
   </Box>
 )
 const mediumBox = (
-  <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+  <Box size="medium" style={{backgroundColor: 'pink'}}>
     medium pink box
   </Box>
 )
 const largeBox = (
-  <Box className="box--large" style={{backgroundColor: 'orange'}}>
+  <Box size="large" style={{backgroundColor: 'orange'}}>
     large orange box
   </Box>
 )
